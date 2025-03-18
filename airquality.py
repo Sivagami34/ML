35545a717eafeef0a15f244db6ecd76e687fced7
Sivagami34 <ksivagami2u@gmail.com>
2025-03-18 16:45:28 +0000

    Update airquality.py

--- airquality.py
+++ airquality.py
@@ -2,10 +2,17 @@ import numpy as np
 import pandas as pd
 d = pd.read_csv("C:\\Users\\sivag\\github-classroom\\Saracens-High-School\\Jetlearn\\ML\\AirQuality.csv")
 d.info()
+d.drop(columns = ["Unnamed: 15", "Unnamed: 16"], inplace = True)
+print(d.isnull().sum())
+d.dropna(inplace = True)
 X = d[["PT08.S1(CO)", "PT08.S5(O3)"]]
 y = d["T"]
+print(y)
+y = y.str.replace(",", ".")
+y = y.astype(float)
+print(y)
 from sklearn.model_selection import train_test_split
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size = 0.6, random_state = 10)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size = 0.7, random_state = 10)
 from sklearn.preprocessing import PolynomialFeatures
 f = PolynomialFeatures(3)
 pXtrain = f.fit_transform(Xtrain)
@@ -17,3 +24,9 @@ pred = lr.predict(pXtest)
 from sklearn.metrics import root_mean_squared_error
 error = root_mean_squared_error(ytest, pred)
 print(error)
+
+llr = LinearRegression()
+llr.fit(Xtrain, ytrain)
+prtt = llr.predict(Xtest)
+e = root_mean_squared_error(ytest, prtt)
+print(e)
